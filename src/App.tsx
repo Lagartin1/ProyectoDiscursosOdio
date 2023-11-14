9145c28db9e43b6fdbd0865f42356708d3054c45
Lagartin1 <andreslabrin10@gmail.com>
2023-11-14 17:36:55 -0300

    Template inicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import "./normalize.css"
+import './App.css'
+
+function App() {
+
+  return (
+    <> 
+    <h1> Test 1</h1>
+    </>
+  )
+}
+
+export default App
