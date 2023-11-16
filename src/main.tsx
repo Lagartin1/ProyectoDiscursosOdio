f1d5ceb9d8f5bed45f00499aa6baf8851e3ffcaf
Lagartin1 <andreslabrin10@gmail.com>
2023-11-16 01:09:16 -0300

    landing Page

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,31 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+// 1. Import `extendTheme`
+import { extendTheme } from "@chakra-ui/react"
+
+// 2. Call `extendTheme` and pass your custom values
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: '#4A5759',
+      },
+    },
+  },
+});
+
+export default theme;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  
+  <ChakraProvider theme={theme}>
+    <BrowserRouter>
       <App />
-  </BrowserRouter>,
+    </BrowserRouter>
+    </ChakraProvider>
+   
 )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,61 @@
+import React from 'react'
+import "./Home.css"
+import { Stack,Radio, RadioGroup ,Input,Img,Box} from '@chakra-ui/react'
+
 function Home() {
+    const [value, setValue] = React.useState('');
+    const [urlSend,setUrlSend] = React.useState('');
+    const handelChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUrlSend(event.target.value);
+      };
+      const handelClickS=() =>{
+        if ((value === null || value === '') && (urlSend === null || urlSend === '')){
+            null
+        }
+        console.log(urlSend)
+    }
     return( 
-        <div>
-            <h1> Home </h1>
-        </div>
+        <>
+            <div className="inputCard">
+                <div className="stackWrapper">
+                    <h3 className='titleSelect' >Selecciona la Red Social:</h3>
+                    <RadioGroup colorScheme={'red'} marginLeft={'2rem'}onChange={setValue} value={value}>
+                        <Stack  color={'#4A5759'} gap={'100px'}className='menuR' direction='row'>
+                            <Radio value='I'>Instagram</Radio>
+                            <Radio value='T'>Twitter (X)</Radio>
+                            <Radio value='F'>Facebook</Radio>
+                        </Stack>
+                    </RadioGroup>
+                </div>
+                <div className='inputField'>
+                    <Input className='inputBox'value={urlSend} focusBorderColor={'#fff'} color={'#4A5759'} _hover={{borderColor:'none'}} borderWidth={'1px' } borderColor='#4A5759' onChange={handelChange} marginTop='2rem' size='md' width='40rem' variant='outline' placeholder='Url' />
+                    <button className='sendB'onClick={handelClickS}> Send</button>
+                </div>
+            </div>
+            <section className="aboutApp">
+               <div className="explanation">
+                    <h1>About</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipiscing elit, aliquam arcu curabitur porta sagittis imperdiet, dapibus dui commodo proin aenean in.
+                        Accumsan vulputate orci sed dui tempor feugiat in rutrum mi risus nisl, a himenaeos maecenas massa enim nec aliquet faucibus nibh scelerisque.
+                        Auctor fermentum eu dapibus nec consequat inceptos facilisi accumsan arcu, fusce magna velit nascetur facilisis venenatis diam sollicitudin urna
+                        vehicula, primis est leo cubilia curabitur justo vitae dui.
+                    </p>
+                    <p>
+                        Sociis sapien pretium ligula ullamcorper eu aliquet vitae fermentum, magnis non per est diam mus cursus, donec sagittis condimentum fames ad quisque 
+                        aenean. Tellus pulvinar nisi dui vitae eleifend penatibus purus iaculis, quisque porta felis convallis nunc ac praesent vulputate lacinia, rhoncus 
+                        congue suspendisse vestibulum cursus varius neque. Platea et posuere purus venenatis vivamus habitant ante suspendisse ut congue cras leo, justo in 
+                        nam tristique enim mauris inceptos diam massa felis litora.
+                    </p>
+               </div>
+               <div className="others">
+                <Box  height='450px'margin={'0.5rem 15rem 0.5rem'} borderRadius={'5rem'} overflow={'hidden'} bg='#B0C4B1;'>
+                    <Img marginLeft={'1rem'} boxSize={'500px'}src='https://upload.wikimedia.org/wikipedia/commons/e/ee/Pirámide_del_odio.svg' alt='Piramide De odio' />
+                </Box>
+               </div>
+
+            </section>
+        </>
     );
 }
 
